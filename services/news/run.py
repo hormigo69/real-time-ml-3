0f51c489541d4b200d2a18a9501e173ca51ebc9e
Hormigo <hormigo@gmail.com>
2024-12-24 19:20:26 +0100

    Dockerize the news service
    Improved the pre-commit hooks

--- services/news/run.py
+++ services/news/run.py
@@ -30,7 +30,7 @@ def main(
     sdf = app.dataframe(source=news_source)
 
     # Let's printing to check if this thing works
-    sdf.print(metadata=True)
+    # sdf.print(metadata=True)
 
     # Push the news to the output topic
     sdf = sdf.to_topic(output_topic)
