8819f280ded52bf699e3237e5bd7827a19eac61f
Hormigo <hormigo@gmail.com>
2024-12-27 10:23:40 +0100

    Change output format of news-signal service

--- services/news-signal/llms/base.py
+++ services/news-signal/llms/base.py
@@ -1,52 +1,68 @@
 from abc import ABC, abstractmethod
-from pydantic import BaseModel, Field
 from typing import Literal
 
-# Define the format that either, the Claude or the Ollama model will follow
+from pydantic import BaseModel, Field
+
+
+class NewsSignalOneCoin(BaseModel):
+    """
+    This class describes the format of the output of the Claude or Ollama model.
+    """
+
+    coin: Literal[
+        "BTC",
+        "ETH",
+        "SOL",
+        "XRP",
+        "DOGE",
+        "ADA",
+        "XLM",
+        "LTC",
+        "BCH",
+        "DOT",
+        "XMR",
+        "EOS",
+        "XEM",
+        "ZEC",
+        "ETC",
+        "XLM",
+        "LTC",
+        "BCH",
+        "DOT",
+        "XMR",
+        "EOS",
+        "XEM",
+        "ZEC",
+        #'ETC',
+    ] = Field(description="The coin that the news is about")
+    signal: Literal[1, -1] = Field(
+        description="""
+        The impact of the news on the coin price.
+        1 if the prices is expected to go up,
+        -1 if it is expected to go down.
+
+        If the news is not related to the coin, no need to create a NewsSignal.
+    """
+    )
 
 
 class NewsSignal(BaseModel):
     """
-    This class describes the format of the output of the Claude model.
+    This class describes the format of the output of the Claude or Ollama model.
     """
 
-    btc_signal: Literal[1, 0, -1] = Field(
-        description="""
-        The impact of the news on the BTC price.
-        1 if the prices is expected to go up,
-        0 if it is expected to stay the same,
-        -1 if it is expected to go down.
-
-        If the news is not related to BTC, the signal should be 0.
-        """
-    )
-
-    eth_signal: Literal[1, 0, -1] = Field(
-        description="""
-        The impact of the news on the ETH price.
-        1 if the prices is expected to go up,
-        0 if it is expected to stay the same,
-        -1 if it is expected to go down.
-
-        If the news is not related to ETH, the signal should be 0.
-        """
-    )
-
-    reasoning: str = Field(
-        description="""
-        The reasoning behind the btc_signal and eth_signal extracted from the news article.
-        """
-    )
+    news_signals: list[NewsSignalOneCoin]
 
     def to_dict(self) -> dict:
         """
         Return a dictionary representation of the NewsSignal object.
         """
-        return {
-            "btc_signal": self.btc_signal,
-            "eth_signal": self.eth_signal,
-            "reasoning": self.reasoning,
-        }
+        # return {
+        #     "btc_signal": self.btc_signal,
+        #     "eth_signal": self.eth_signal,
+        #     "reasoning": self.reasoning,
+        # }
+        raise NotImplementedError()
 
 
 class BaseNewsSignalExtractor(ABC):
--- services/news-signal/llms/claude.py
+++ services/news-signal/llms/claude.py
@@ -20,22 +20,24 @@ class ClaudeNewsSignalExtractor(BaseNewsSignalExtractor):
 
         self.prompt_template = PromptTemplate(
             template="""
-            You are a financial analyst.
-            You are given a news article and you need to determine the impact of the news on the BTC and ETH prices.
+            You are an expert crypto financial analyst with deep knowledge of market dynamics and sentiment analysis.
+            Analyze the following news story and determine its potential impact on crypto asset prices.
+            Focus on both direct mentions and indirect implications for each asset.
 
-            You need to output the signal in the following format:
-            {
-                "btc_signal": 1,
-                "eth_signal": 0
-            }
+            Do not output data for a given coin if the news is not relevant to it.
 
-            The signal is either 1, 0, or -1.
-            1 means the price is expected to go up.
-            0 means the price is expected to stay the same.
-            -1 means the price is expected to go down.
+            ## Example input
+            "Goldman Sachs wants to invest in Bitcoin and Ethereum, but not in XRP"
 
-            Here's the news article:
-            {news_article}
+            ## Example output
+            [
+                {"coin": "BTC", "signal": 1},
+                {"coin": "ETH", "signal": 1},
+                {"coin": "XRP", "signal": -1},
+            ]
+
+            News story to analyze:
+            {news_story}
             """
         )
 
@@ -44,12 +46,12 @@ class ClaudeNewsSignalExtractor(BaseNewsSignalExtractor):
     def get_signal(
         self,
         text: str,
-        output_format: Literal["dict", "NewsSignal"] = "dict",
+        output_format: Literal["dict", "NewsSignal"] = "NewsSignal",
     ) -> NewsSignal | dict:
         response: NewsSignal = self.llm.structured_predict(
             NewsSignal,
             prompt=self.prompt_template,
-            news_article=text,
+            news_story=text,
         )
 
         if output_format == "dict":
